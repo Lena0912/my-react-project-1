a810bedf140234b46927e3e5789f4292cf1af7e6
Lena <elenabelichenko9@gmail.com>
2024-12-26 01:21:39 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-react-project-1/",
   plugins: [react()],
   build: {
     sourcemap: true,
